669eb2813d38e4186c9488c8e42b592b8adfc9d3
Tiffany Vallo <tiffanyvallo@tiffanys-mbp.home>
2021-04-19 16:44:42 +0100

    Gem file updated -resolved rubocop issue

--- Gemfile
+++ Gemfile
@@ -2,8 +2,6 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
-ruby '3.0.0'
-
 gem 'sinatra'
 
 group :test do
--- app.rb
+++ app.rb
@@ -1,10 +1,6 @@
 require 'sinatra/base'
-require 'sinatra/reloader'
 
 class BookmarkManager < Sinatra::Base
-  configure :development do
-    register Sinatra::Reloader
-  end
 
   get '/' do
     erb :index
