8a5fe93e78efc82d620c508e43973e761394e116
Tiffany Vallo <tiffanyvallo@tiffanys-mbp.home>
2021-04-19 16:33:33 +0100

    Index erb file created

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,6 +1,6 @@
 feature 'Hello World' do
   scenario 'it prints Hello World' do
     visit('/')
-    expect(page).to have_content 'Hello World'
+    expect(page).to have_content 'Bookmark Manager'
   end
 end
